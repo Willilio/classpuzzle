26a84777cf39c30ab1f9f1e2174098bdec506665
Will Irwin <wirwin@minitab.com>
2025-07-15 10:53:48 -0400

    update lint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,17 @@
 module.exports = {
   'env': {
     'browser': true,
-    'es2021': true,
     'node': true
   },
   'extends': 'eslint:recommended',
   'parserOptions': {
     'ecmaVersion': 12,
+    'ecmaFeatures': {
+      'jsx': true,
+    },
     'sourceType': 'module'
   },
+  'plugins': ['react'],
   'rules': {
     'quotes': ['error', 'single'],
     // we want to force semicolons
@@ -17,5 +20,10 @@ module.exports = {
     'indent': ['error', 2],
     // we want to avoid extraneous spaces
     'no-multi-spaces': ['error']
-  }
-}
+  },
+  'settings': {
+    'react': {
+      'version': 'detect',
+    },
+  },
+};
