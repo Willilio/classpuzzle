6c348e99ad625500e4cf1a0cb40ef056f1dc984a
Will Irwin <wirwin@minitab.com>
2025-07-28 10:41:50 -0400

    square state updates on click

--- src/components/board/PuzzleBoard.jsx
+++ src/components/board/PuzzleBoard.jsx
@@ -1,25 +1,36 @@
 import { useMemo } from 'react';
 import PuzzleRow from './PuzzleRow';
+import { useDispatch } from 'react-redux';
+import { cycleSquare } from '../../services/slices/boardState';
+
+const NUM_BOARD_ROWS = 6;
 
 /**
  * The Puzzle Board contains the puzzle itself
  * @param {{boardState: [number]}} props
  */
 const PuzzleBoard = ({ boardState }) => {
+  const dispatch = useDispatch();
+
   const rowValues = useMemo(() => {
     const lol = [];
-    for (let i = 0; i < 6; i++) {
-      lol.push(boardState.slice(i*6, (i+1)*6));
+    for (let i = 0; i < NUM_BOARD_ROWS; i++) {
+      lol.push(boardState.slice(i*NUM_BOARD_ROWS, (i+1)*NUM_BOARD_ROWS));
     }
     console.log(lol);
     return lol;
   }, [boardState]);
 
+  const onSquareClicked = (row, column) => {
+    const squareIndex = row * NUM_BOARD_ROWS + column;
+    dispatch(cycleSquare({ squareID: squareIndex, direction: 1 }))
+  }
+
   return (
     <div className="classpuzzle-container">
       <div className="classpuzzle-board">
-        {rowValues.map(states => (
-          <PuzzleRow squareStates={states} />
+        {rowValues.map((states, index) => (
+          <PuzzleRow squareStates={states} onRowClick={column => onSquareClicked(index, column)} />
         ))}
       </div>
     </div>
